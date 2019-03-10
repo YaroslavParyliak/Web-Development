e9e7f7764f4e60e7910941038f5e0d4b5f8966f3
Yaroslav Paryliak <jaroslavpariljak@gmail.com>
2019-03-10 15:29:01 +0200

    Add files via upload

---
+++ lab1/script.js
@@ -0,0 +1,11 @@
+var square = function(number) { return number * 8; };
+var x = square(2); 
+console.log(x);
+
+function reverseStr(str) {
+    
+        return str.split("").reverse().join("");
+    
+    }
+   
+    console.log(reverseStr("Привет мир!"));
